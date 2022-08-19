a3d484b2710d8bc4a7ffdea92bc99fe02753acb4
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-19 11:55:11 +0000

    start good weather app

---
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Mary Moreira on 19/08/2022.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Mary Moreira on 19/08/2022.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+}
