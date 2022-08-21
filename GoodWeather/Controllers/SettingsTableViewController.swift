e5960a6986ca3079d0bafca2ac1d959181937b70
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-21 18:03:41 +0000

    add settings screen

---
+++ GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsTableViewController.swift
+//  GoodWeather
+//
+//  Created by Mary Moreira on 21/08/2022.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+    private var settingsViewModel = SettingsViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsViewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+        let settingsItem = settingsViewModel.units[indexPath.row]
+        cell.textLabel?.text = settingsItem.displayName
+        return cell
+    
+    }
+}
---
+++ GoodWeather/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsViewModel.swift
+//  GoodWeather
+//
+//  Created by Mary Moreira on 21/08/2022.
+//
+
+import Foundation
+
+enum Unit: String, CaseIterable {
+    case celsius = "metric"
+    case fahrenheit = "imperial"
+}
+
+extension Unit {
+    
+    var displayName: String {
+        get {
+            switch(self) {
+            case .celsius:
+                return "Celsius"
+            case .fahrenheit:
+                return "Fahrenheit"
+            }
+        }
+    }
+}
+
+class SettingsViewModel {
+    
+    let units = Unit.allCases
+    
+    var selectedUnit: Unit {
+        get {
+            let userDefault = UserDefaults.standard
+            var unitValue = ""
+            if let value = userDefault.value(forKey: "unit") as? String {
+                unitValue = value
+            }
+            return Unit(rawValue: unitValue) ?? Unit.celsius
+        }
+        set {
+            let userDefault = UserDefaults.standard
+            userDefault.set(newValue.rawValue, forKey: "unit")
+        }
+    }
+    
+}
